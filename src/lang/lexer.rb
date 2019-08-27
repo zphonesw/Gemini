3d51ac2220d8be9315d097ebc435020364d72ddf
zphonesw <49452808+zphonesw@users.noreply.github.com>
2019-08-27 17:08:05 -0500

    Update lexer.rb

--- src/lang/lexer.rb
+++ src/lang/lexer.rb
@@ -17,6 +17,26 @@ class Lexer
     
     while i < code.size 
       chunk = code[i..-1]
+      
+      if identifier = chunk[\A([a-z]\w*), 1] then
+        if KEYWORDS.include?(identifier) then
+          tokens << [identifier.upcase.to_sym, identifier]
+        else
+          tokens << [:IDENTIFIER, identifier]
     end
+    i += identifier.size
+        elsif constant = chunk[\A([A-Z]\w*), 1] 
+          tokens << [:CONSTANT, constant]
+          i += constant.size
+          
+        elseif number = chunk[\A([0-9]+), 1]
+          tokens << [:NUMBER, number]
+          i += number.size
+          
+        elseif string = chunk[\A"(.*?)", 1]
+          tokens << [:STRING, string]
+          i += string.size + 2
+          
+        elseif indent = chunk[\A\:\n( +)/m, 1]
   end
 end
