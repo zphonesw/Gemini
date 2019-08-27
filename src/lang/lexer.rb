897804519c563e864602fa3ce932d48731ddc777
zphonesw <49452808+zphonesw@users.noreply.github.com>
2019-08-27 15:24:21 -0500

    Update lexer.rb

--- src/lang/lexer.rb
+++ src/lang/lexer.rb
@@ -1,8 +1,22 @@
-require 'gemini'
-require 'set'
+require "tokens"
 
-module Gemini
-  class Lexer
+class Lexer
+  KEYWORDS = ["fun","holder","stdout","TRUE","FALSE","nil"]
+  
+  def tokenize(code)
     
+    code.chomp!
+    
+    i = 0
+    
+    tokens = []
+    
+    current_indent = 0
+    
+    indent_stack = []
+    
+    while i < code.size 
+      chunk = code[i..-1]
+    end
   end
 end
