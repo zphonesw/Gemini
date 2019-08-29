3ec941465bf6fd2c1222325f8fdf4537e0a7f1be
zphonesw <49452808+zphonesw@users.noreply.github.com>
2019-08-29 08:49:30 -0500

    Update gemini.rb

--- src/gemini.rb
+++ src/gemini.rb
@@ -1,5 +1,16 @@
 module Gemini
+  $tokens = []
   class Lexer
-    attr_accessor
-  end
+            attr_accessor :code
+            def initialize(code)
+                @code = code
+            end
+            #This method is responsible for breaking the code down into the smallest units as possible.
+            def tokenize
+                for c in @code.split(" ").map(&:to_s)
+                $tokens << getToken
+                @code = @code.strip
+            end
+            $tokens
+        end
 end
